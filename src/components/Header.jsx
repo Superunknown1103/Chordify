fece34dc59a2b3d4ed34a235a80a8b6eefb5222a
Superunknown1103 <superunknown1103@gmail.com>
2017-10-26 18:19:57 -0500

    custom CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,10 @@ class Header extends Component {
         return (
             <header>
                 <h1>Chordify</h1>
+                <h3>Welcome to Chordify, the ultimate tool for efficient songwriting.
+                Type the lyrics to your song, add square brackets to insert a chord,
+                and boom you have yourself a chord chart. Thanks to a wonderful NPM
+                package called ChordSheetJS, this is all possible.</h3>
                 </header>
         );
     }
