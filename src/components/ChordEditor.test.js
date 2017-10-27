f1aa6067b47d8c246ecfb19ca9d8d73e4d865bfc
Superunknown1103 <superunknown1103@gmail.com>
2017-10-26 19:00:11 -0500

    added yarn and more tests

--- src/components/ChordEditor.test.js
+++ src/components/ChordEditor.test.js
@@ -7,4 +7,9 @@ it('renders an editor area', () => {
   const editor = shallow(<ChordEditor />);
   expect(editor.find('textarea').length).toEqual(1);
 });
+
+it('renders an output area', () => {
+  const editor = shallow(<ChordEditor />);
+  expect(editor.find('div.chord-output').length).toEqual(1);
+})
 });
